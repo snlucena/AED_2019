2bdba1a0dd7f88479571e1c7c127d37c785ceff5
snlucena <37759376+snlucena@users.noreply.github.com>
2019-05-16 21:29:47 -0300

    upload files

---
+++ 00-HelloWorld/hello_world.cpp
@@ -0,0 +1,12 @@
+/* Hello World
+probar el compilador con un programa Hello World y capturar su salida en un archivo .txt 
+Sofia Lucena
+06042018
+*/
+
+#include <iostream>
+
+int main () {
+	
+	std::cout << "Hello World, it's a brand new day\n";
+}
